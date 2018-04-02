5ccbf72b5f5879a73a87d8372fcd0f96cce41027
velafarq <vela.mrdjen@gmail.com>
2018-04-02 18:53:27 -0400

    began fetch function for beverage search

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+//variables to access DOM//
+
 const drinkBlock = document.getElementById('drink'),
 appetizerBlock = document.getElementById('appetizer'),
 entreeBlock = document.getElementById('entree'),
 dessertBlock = document.getElementById('dessert'),
 searchWord = document.getElementById('search-word'),
-searchForm = document.getElementById('search-form');
+searchForm = document.getElementById('search-form'),
+
+//API endpoint url variables//
+
+yummlyUrl = 'https://api.yummly.com/v1',
+edamamUrl = 'https://api.edamam.com/search';
 
 searchForm.addEventListener('submit', getResults);
 
@@ -11,5 +18,21 @@ function getResults(e) {
   e.preventDefault();
   let inputText = searchWord.value;
   searchWord.value = '';
-  
+
+  fetchDrink(inputText);
+}
+
+function fetchDrink(inputText) {
+  fetch(`${yummlyUrl}/api/recipes?_app_id=33ff0359&_app_key=c9dbab1cb989f66ccb3e9f085c6fe142&
+  q=${inputText}&requirePictures=true`)
+  .then(res => res.json())
+  .then(data => {
+    let output = '';
+    output += `
+    <img src='${data.matches[0].imageUrlsBySize[0]}'>
+    <h3>${data.matches[0].recipeName}<h3>
+    <h4>Ingredients</h4>
+    <p>${data.matches[0].ingredients}`;
+  });
+  drinkBlock.innerHTML = output;
 }
