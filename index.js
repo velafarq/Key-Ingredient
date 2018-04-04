bb8afc2f472b8944ddf8f3504fc61a16511d597c
velafarq <vela.mrdjen@gmail.com>
2018-04-04 14:58:19 -0400

    Add API URLs and confirm basic API functionality

--- index.js
+++ index.js
@@ -17,22 +17,35 @@ searchForm.addEventListener('submit', getResults);
 function getResults(e) {
   e.preventDefault();
   let inputText = searchWord.value;
-  searchWord.value = '';
+
 
   fetchDrink(inputText);
 }
 
+function fetchAppetizer(inputText) {
+  fetch(`${edamamUrl}?q=${inputText}&app_id=fddbf388&app_key=
+87b9baad36b2f49560c619843a13a7c0`)
+.then(res => res.json())
+.then(data => console.log(data))
+.catch(err => console.log(err));
+}
+
 function fetchDrink(inputText) {
-  fetch(`${yummlyUrl}/api/recipes?_app_id=33ff0359&_app_key=c9dbab1cb989f66ccb3e9f085c6fe142&
-  q=${inputText}&requirePictures=true`)
+  fetch(`${yummlyUrl}/api/recipes?_app_id=33ff0359&_app_key=c9dbab1cb989f66ccb3e9f085c6fe142&q=${inputText}&allowedCourse[]=course^course-Beverages&requirePictures=true`)
   .then(res => res.json())
   .then(data => {
     let output = '';
     output += `
-    <img src='${data.matches[0].imageUrlsBySize[0]}'>
-    <h3>${data.matches[0].recipeName}<h3>
-    <h4>Ingredients</h4>
-    <p>${data.matches[0].ingredients}`;
-  });
-  drinkBlock.innerHTML = output;
+  <img src='${data.matches[0].imageUrlsBySize['90']}'>
+  <h3>${data.matches[0].recipeName}<h3>
+  <h4>Ingredients</h4>
+  <p>${data.matches[0].ingredients}</p>
+  <a href='https://www.yummly.com/recipe/${data.matches[0].id}' target='_blank'>Take me to the recipe</a>`;
+
+    drinkBlock.innerHTML = output;
+
+  })
+  .catch(err => console.log(err));
+
+
 }
